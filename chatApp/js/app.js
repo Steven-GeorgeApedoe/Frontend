d1dd8885debc02c39f28cc62e49f58279f9ce213
Steven-GeorgeApedoe <stevenapedoe@gmail.com>
2024-08-16 18:09:47 +0000

    New changes

--- chatApp/js/app.js
+++ chatApp/js/app.js
@@ -1,17 +1,55 @@
-const styboSelectorBtn = document.querySelector('#stybo-selector')
-const ishaSelectorBtn = document.querySelector('#mimi-selector')
+// function appMessenger(client, server, Figma) {
+//     this.client = client;
+//     this.server = server;
+//     this.Figma = Figma;
+// }
+//
+// let message1 = new appMessenger("asBank", "centinel", "architecture");
+// let message2 = new appMessenger("utBank", "Ugo", "design");
+// let messaage3 = new appMessenger("global", "DMZ", "logical");
+//
+// console.log("message: "+ message1.client + " = "+ message1.server);
+const johnSelectorBtn = document.querySelector('#john-selector')
+const janeSelectorBtn = document.querySelector('#jane-selector')
 const chatHeader = document.querySelector('.chat-header')
-const chatMessages = document.querySelector('chat-messages')
-const chatInputForm = document.querySelector('chat-input-form')
-const chatInput = document.querySelector('chat-input')
-const clearChatBtn = document.querySelector('clear-chat-button')
+const chatMessages = document.querySelector('.chat-messages')
+const chatInputForm = document.querySelector('.chat-input-form')
+const chatInput = document.querySelector('.chat-input')
+const clearChatBtn = document.querySelector('.clear-chat-button')
 
-const chatMessageElement = (message) => 
+const chatMessageElement = (message) =>
 <div class= "message ${message.sender === 'Stybo' ? 'blue-bg' : 'gray-bg'}">
-<div class= "message-sender">${message.sender}</div>
-<div class = "messsage-text">${message.text}</div>
-<div class="message-timestamp">${message.timestamp}</div>
+  <div class= "message-sender">${message.sender}</div>
+  <div class= "message-text">${message.text}</div>
+  <div class= "message-timestamp">${message.timestamp}</div>
 </div>
 
 
+let messageSender = 'John';
+const updateMessageSender = (name) => {
+  messageSender = name;
+  chatHeader.innerText = `${messageSender} chatting...`
+  chatInput.placeholder = `Type here, ${messageSender}`
+}
+if (name === 'John'){
+  johnSelectorBtn.classList.add('active-person')
+  janeSelectorBtn.classList.remove('active-person')
+}
 
+if (name === 'Mimi'){
+  styboSelectorBtn.classList.add('active-person')
+  mimiSelectorBtn.classList.add('active-person')
+}
+
+styboSelectorBtn.onclick = () => updateMessageSender('Stybo')
+mimiSelectorBtn.onclick =  () => updateMessageSender('Mimi')
+const sendMessage = (e) => {
+  e.preventDefault()
+
+  const timestamp = new Date().toLocaleDateString('en-Us', {hour: 'numeric', minute: 'numeric', hour12: true})
+  const message = {
+    sender: '',
+    text: chatInput.value,
+    timestamp,
+  }
+}
---
+++ chatApp/js/pointAndKill.js
@@ -0,0 +1,15 @@
+function student(name, sex, heightInFeet, weightInKilogram){
+  this.name = name;
+  this.sex = sex;
+  this.heightInFeet = heightInFeet;
+  this.weightInKilogram = weightInKilogram;
+}
+
+const student1 = new student("Selene", "Female", 5.5, 74);
+const student2 = new student("George", "Male", 6, 80);
+const student3 = new student("Demetri", "Male", 5.8, 68 );
+const Student4 = new student("Monique", "Female", 5.7, 63)
+const Student5 = new student("Emily", "female", 5.11, 58)
+
+console.log(student1.name+ " is "+ student1.sex+ " and "+student1.heightInFeet+ " in feet")
+
